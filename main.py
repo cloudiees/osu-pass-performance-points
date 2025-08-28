edc2f891fc14b635f069f48f7f78ed1210d13da5
cloudiees <73859729+cloudiees@users.noreply.github.com>
2025-08-28 01:13:01 -0500

    Added activity message

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import asyncio
 import time
 from auto_scan import scan_recent
 from bot import bot
+import discord
 
 
 @bot.event
 async def on_ready():
+    activity = discord.Activity(type=discord.ActivityType.playing, name="/help for help")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
     await bot.tree.sync()
     print(f"Logged in as {bot.user.name}")
     if not hasattr(bot, 'task_started'):
